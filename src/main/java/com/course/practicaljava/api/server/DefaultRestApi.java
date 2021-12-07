9a55fa1716314cb50e8139aa4fba5f430486608d
prab99@yahoo.com <prab99@yahoo.com>
2021-12-07 21:36:08 +0800

    added logger

--- src/main/java/com/course/practicaljava/api/server/DefaultRestApi.java
+++ src/main/java/com/course/practicaljava/api/server/DefaultRestApi.java
@@ -3,6 +3,8 @@ package com.course.practicaljava.api.server;
 import java.time.LocalTime;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
 
+	private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
 	@GetMapping(value = "/welcome")
 	public String welcome() {
-		System.out.println(StringUtils.join("Hello! ", "This is ", "Spring Boot ", "API "));
+		LOG.info(StringUtils.join("Hello! ", "This is ", "Spring Boot ", "API "));
 		return "Welcome to Prabhat test Spring Boot API";
 	}
 	@GetMapping(value = "/time")
